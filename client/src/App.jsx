444f380ffe04e5bedb2e5af502ecf255eef397d2
Angel Aguirre Obando <anngelao25@gmail.com>
2023-11-08 15:42:04 -0500

    Add routing and post show page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
-import PostsList from "./features/posts/PostsList";
+import AppRoutes from "./components/AppRoutes";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
 
-    <>
+    <Router>
       <div className="app">
         <h1>React on Raisl Blog</h1>
         <p>Find this application layout in client/src/App.jsx</p>
-        <PostsList />
+        <NavBar/>
+        <AppRoutes />
       </div>
-    </>
+    </Router>
   );
 }
 
