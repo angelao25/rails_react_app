6b04e13bff6ee5374af43b83e8c5af4db8351677
Angel Aguirre Obando <anngelao25@gmail.com>
2024-02-14 09:31:53 -0500

    Implemented edit functionality

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+
 import NewPostForm from "../features/posts/NewPostForm";
 import PostDetails from "../features/posts/PostDetails";
+import PostEditForm from "../features/posts/PostEditForm";
 import PostsList from "../features/posts/PostsList";
-
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<PostsList />} />
             <Route path="posts/:id" element={<PostDetails />} />
+            <Route path="/posts/:id/edit" element={<PostEditForm/>} />
             <Route path="/new" element={<NewPostForm />} />
         </Routes>
     );
