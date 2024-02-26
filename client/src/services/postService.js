ff309e418f2b914adeb9716d74f34c64f412fd2c
Angel Aguirre Obando <anngelao25@gmail.com>
2024-02-26 15:39:57 -0500

    Finished the refactor

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -17,6 +17,36 @@ async function fetchPost(id) {
 
 }
 
+async function createPost(postData) {
+    const response = await fetch(`${API_URL}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    return response.json();
+}
+
+async function updatePost(id, postData) {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData),
+    })
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response.json();
+}
+
 async function deletePost(id) {
     const response = await fetch(`${API_URL}/${id}`, {
         method: "DELETE",
@@ -34,5 +64,5 @@ async function deletePost(id) {
     return response.json();
 }
 
-export { deletePost, fetchAllPosts, fetchPost };
+export { createPost, deletePost, fetchAllPosts, fetchPost, updatePost };
 
