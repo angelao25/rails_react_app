45085fd2cbfd75712ae08022a624d8db40a773c9
Angel Aguirre Obando <anngelao25@gmail.com>
2024-03-05 12:03:08 -0500

    Finished

--- client/src/hooks/useURLSearchParam.js
+++ client/src/hooks/useURLSearchParam.js
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function useURLSearchParam(paramName, initialValue = "") {
-    const query = new URLSearchParams(window.location.search);
-    const[paramValue, setParamValue] = useState(
-        query.get(paramName) || initialValue
-    );
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    useEffect(() => {
-        const newURL = paramValue
-            ? `${window.location.pathname}?${paramName}=${paramValue}`
-            : window.location.pathname;
-        window.history.pushState({}, "", newURL);
-    }, [paramValue, paramName]);
+    // DIrectly get the value from searchParams
+    const paramValue = searchParams.get(paramName) || initialValue;
+
+    const setParamValue = (value) => {
+        if (value) {
+            // Update only the specific parameter, and preserve the others.
+            setSearchParams({...Object.fromEntries(searchParams), [paramName]: value});
+        } else {
+            // Remove the parameter if its value is falsy.
+            searchParams.delete(paramName);
+            setSearchParams(searchParams);
+        }
+    };
 
     return [paramValue, setParamValue];
 }
